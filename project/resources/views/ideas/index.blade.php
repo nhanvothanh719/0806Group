55925775b851ab17319b9dfa55f739956598971d
Vo Thanh Nhan <nhanvtgcd191366@fpt.edu.vn>
2022-03-19 11:06:42 +0700

    Modify idea index page

--- project/resources/views/ideas/index.blade.php
+++ project/resources/views/ideas/index.blade.php
@@ -52,60 +52,6 @@
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-9 mt-3">
-            <div class="">
-                <h4 class="title d-inline">LASTEST IDEA</h4>
-                @include('ideas.create')
-                <button class="btn btn-success d-inline float-md-end">My Idea</button>
-            </div>
-            <div class="my-lg-3">
-                @foreach ($ideas as $idea)
-                <div class="card mt-3">
-                    <div class="card-body">
-                        <h5>{{ $idea->title }}</h5>
-                        <small class="text-muted">
-                            Post by: {{ $idea->user->name }}
-                        </small>
-                        <div class="">
-                            <p>
-                                {{ $idea->content }}
-                            </p>
-                        </div>
-                        <div class="activity__list__footer">
-                            <a href="#"> <i class="fa fa-thumbs-up"></i>123</a>
-                            <a href="#"> <i class="fa fa-thumbs-down"></i>123</a>
-                            <a href="#"> <i class="fa fa-comments"></i>{{ $idea->comments_count }}</a>
-                            <span> <i class="fa fa-clock"></i>{{ $idea->created_at->diffForHumans() }}</span>
-                        </div>
-                    </div>
-                    <form action="{{url('/ideas/'.$idea->id)}}" method="GET">
-                        <div class=" timeline-comment-box float-right">
-                            <button type="submit" class="btn btn-primary" onclick="window.location.href='{{url('/ideas/'.$idea->id)}}'">See more</button>
-                        </div>
-                    </form>
-                </div>
-                @endforeach
-                {{ $ideas->links() }}
-            </div>
-        </div>
-        <div class="col-md-3">
-            @include('ideas.search')
-        </div>
-    </div>
-    <nav class="navbar navbar-expand-sm navbar-dark"> <img src="https://i.imgur.com/CFpa3nK.jpg" width="20" height="20" class="d-inline-block align-top rounded-circle" alt=""> <a class="navbar-brand ml-2" href="#" data-abc="true">Rib Simpson</a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-    <div class="end">
-        <div class="collapse navbar-collapse" id="navbarColor02">
-            <ul class="navbar-nav">
-                <li class="nav-item"> <a class="nav-link" href="#" data-abc="true">Work</a> </li>
-                <li class="nav-item"> <a class="nav-link" href="#" data-abc="true">Capabilities</a> </li>
-                <li class="nav-item "> <a class="nav-link" href="#" data-abc="true">Articles</a> </li>
-                <li class="nav-item active"> <a class="nav-link mt-2" href="#" data-abc="true" id="clicked">Contact<span class="sr-only">(current)</span></a> </li>
-            </ul>
-        </div>
-    </div>
-</nav>
 <!-- Main Body -->
 <section>
     <div class="container">
